543e3e24904f864403be90038f5dd6874513c8d7
gitter2020 <gitter2020@example.com>
2021-03-12 21:17:57 -0500

    add more to main

--- cmd/ulid0/main.go
+++ cmd/ulid0/main.go
@@ -19,6 +19,12 @@ func main()  {
 	}
 	ulid1.UnmarshalBinary(bin)
 
+	parse, err := ulid.Parse("0000XSNJG0MQJHBF4QX1EFD6Y3T8CNRGXPSBZSA1PY2YX1SS9GX3RKNSGMVZ81STZM")
+	if err != nil {
+		fmt.Println("Error !")
+	}
+	fmt.Println(parse)
+
 	t := time.Unix(1000000, 0)
 	entropy1 := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
 	fmt.Println(ulid.MustNew(ulid.Timestamp(t), entropy1))
